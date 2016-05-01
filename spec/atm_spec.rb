d9922cbeb0f1ae8585379301ead148b494331a76
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-05-01 17:21:54 +0200

    if not enough funds, reciept implemented

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -16,6 +16,11 @@ describe Atm do
     expect(subject.funds).to eq 950
   end
 
+  it 'reject withdraw if account has insufficient funds' do
+    expected_output = { status: true, message: 'insufficient funds', date: Date.today }
+    expect(subject.withdraw(105, account)).to eq expected_output
+  end
+
   it 'allow withdraw if account has enough balance.' do
        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
        expect(subject.withdraw(45, account)).to eq expected_output
