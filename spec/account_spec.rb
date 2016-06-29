d3b602c2fe51ce0d19c3fe15d3727db7a012b13c
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-06-29 14:51:41 +0200

    added balance test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,8 @@ it 'is expected to have a 4 digit pin number on initialize' do
   pin_length = Math.log10(subject.pin_code).to_i + 1
   expect(pin_length).to eq 4
 end
+
+it 'is expected to a balance of 0 on initialize' do
+  expect(subject.balance).to eq 0
+end
 end
